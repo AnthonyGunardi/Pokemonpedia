81de780f4245cb3ba590b98f85d746dc45f62474
AnthonyGunardi <anthonygunardi@gmail.com>
2021-02-02 00:10:16 +0700

    minor fix on App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '/components/Header'
+import Header from './components/Header'
 import Sidebar from './components/Sidebar'
 import Footer from './components/Footer'
 import PokeDetail from './pages/pokeDetail'
