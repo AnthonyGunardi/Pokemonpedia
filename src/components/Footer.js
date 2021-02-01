7ef480a39ee8ab67b9f3e957705526217431ff43
AnthonyGunardi <anthonygunardi@gmail.com>
2021-02-02 00:02:13 +0700

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="flex justify-center items-center my-3">
+        <p className="text-center">Copyright © 2021 Anthony Gunardi. All Rights Reserved | Author : <a href="https://github.com/AnthonyGunardi" target="_blank">Anthony Gunardi</a> | <a href="https://github.com/AnthonyGunardi/react-challenge" target="_blank">Help</a>
+        </p>
+    </footer>
+    )
+  }
+}
+
+export default Footer
