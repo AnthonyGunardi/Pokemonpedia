542038f706eae2b314d0134fa0e182a7440340a3
AnthonyGunardi <anthonygunardi@gmail.com>
2021-02-04 17:03:40 +0700

    change link on Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer () {
   return (
     <footer className="flex justify-center items-center my-3 mt-5">
-      <p className="text-center">Copyright © 2021 Anthony Gunardi. All Rights Reserved | Author : <a href="https://github.com/AnthonyGunardi" target="_blank" rel="noreferrer" className="link">Anthony Gunardi</a> | <a href="https://github.com/AnthonyGunardi/react-challenge" target="_blank" rel="noreferrer" className="link">Help</a>
+      <p className="text-center">Copyright © 2021 Anthony Gunardi. All Rights Reserved | Author : <a href="https://github.com/AnthonyGunardi" target="_blank" rel="noreferrer" className="link">Anthony Gunardi</a> | <a href="https://github.com/AnthonyGunardi/Pokemonpedia" target="_blank" rel="noreferrer" className="link">Help</a>
       </p>
     </footer>
   )
